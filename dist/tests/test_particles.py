1a12a01396c317dba61c084b4c345799cb3a7858
Brighten Tompkins <brightenqtompkins@gmail.com>
2021-11-30 20:12:19 -0800

    build(github.ci): you know it's the real deal because I am making legitimate commits now
    I ran through and adjusted and tweaked everything I could find that could mess with the continuous integration and hopefully we will be able to push through swinging

--- dist/tests/test_particles.py
+++ dist/tests/test_particles.py
@@ -4,5 +4,6 @@ import pytest
 from viscloudio import particles
 
 
-def test_particle(cloud):
+def test_particles():
   print('Testing particles...')
+  particles.setupViscloudio('../../assets/audio/drum_loop.ogg')
